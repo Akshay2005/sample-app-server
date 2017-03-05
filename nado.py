4e3fc170b55d46419b85b31586b2f6934ef201a3
Akshay Sonvane <akshay.sonvane93@gmail.com>
2017-03-05 14:59:18 -0800

    Initial commit.

--- nado.py
+++ nado.py
@@ -1,33 +1,35 @@
 import os
-
+import base64
 import requests
 import tornado.ioloop
 import tornado.web
 
 
-GEOCODE_FMT = 'https://maps.googleapis.com/maps/api/geocode/json?address={address}&key={key}'
-
-
-class GeocodeHandler(tornado.web.RequestHandler):
-    """Proxy a call to the Google Maps geocode API"""
+class TileHandlar(tornado.web.RequestHandler):
 
     def set_default_headers(self):
         # allow cross-origin requests to be made from your app on DroneDeploy to your web server
         self.set_header("Access-Control-Allow-Origin", "https://www.dronedeploy.com")
         self.set_header("Access-Control-Allow-Headers", "x-requested-with")
         # add more allowed methods when adding more handlers (POST, PUT, etc.)
-        self.set_header("Access-Control-Allow-Methods", "GET, OPTIONS")
+        self.set_header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 
-    def get(self):
-        api_key = os.environ.get("API_KEY")
-        address = self.get_query_argument("address")
-        url = GEOCODE_FMT.format(address=address, key=api_key)
+    def post(self):
+       # decode the request body to JSON
+		data = tornado.escape.json_decode(self.request.body)
 
-        # fetch results of the geocode from Google
-        response = requests.get(url)
+		# list of tile image URLs will be pushed into the following list and sent to client
+		encoded_tiles = []
 
-        # send the results back to the client
-        self.write(response.content)
+		for tile in data['tile']:
+			# get results from the given URL including tile image
+			res = requests.get(tile)
+			# encode the tile png into base64
+			encoded = base64.b64encode(res.content)
+			encoded_tiles.append(encoded)
+
+		# send the results as JSON format back to the client
+		self.write({'msg': encoded_tiles})
 
     def options(self):
         # no body
@@ -37,7 +39,7 @@ class GeocodeHandler(tornado.web.RequestHandler):
 
 def main():
     application = tornado.web.Application([
-        (r"/geocode/", GeocodeHandler)
+        (r"/getUrl/", TileHandlar)
     ])
     port = int(os.environ.get("PORT", 5000))
     application.listen(port)
